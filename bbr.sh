2f1477697c82fd0eb0669f3fb41af8cc7b5a0767
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2019-01-22 22:17:12 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -34,10 +34,9 @@ bit=`uname -m`
 dir=`pwd`
 installbbr(){
 	#Install GCC
-	cat >> /etc/apt/sources.list << TEMPEOF
+	echo "
 	deb http://ftp.us.debian.org/debian/ jessie main contrib non-free
-	deb-src http://ftp.us.debian.org/debian/ jessie main contrib non-free
-	TEMPEOF
+	deb-src http://ftp.us.debian.org/debian/ jessie main contrib non-free" >> /etc/apt/sources.list 
 	apt-get update
 	apt-get install build-essential -y
 	apt-get install make gcc-4.9 g++-4.9 g++-4.9-multilib make libelf-dev -y
